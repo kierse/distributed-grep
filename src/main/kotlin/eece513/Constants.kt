31f1f05315fd88cd33facd82e3d0cf9508f49325
Kieran Elliott <kierse@pissiphany.com>
2018-01-21 18:27:13 -0800

    cleaned up constants file

--- src/main/kotlin/eece513/Constants.kt
+++ src/main/kotlin/eece513/Constants.kt
@@ -1,14 +1,9 @@
 package eece513
 
-import org.pmw.tinylog.Level
-import java.net.InetAddress
-
-val SERVER_IP: InetAddress = InetAddress.getByName("ec2-35-183-26-44.ca-central-1.compute.amazonaws.com")
 const val SERVER_PORT = 6969
 
 const val GREP_CMD = "grep"
 
 const val SERVER_LOG_LOCATION = "logs/grep-server.log"
 const val CLIENT_LOG_LOCATION = "logs/grep-client.log"
-const val GREP_DATA_LOCATION = "machine.1.log"
 
